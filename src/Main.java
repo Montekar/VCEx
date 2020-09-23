2fdadd9050d13079d1b533b796ccaf5e89702978
DariusTarvydas <71546374+DariusTarvydas@users.noreply.github.com>
2020-09-23 13:05:11 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Change me if you can");
+        System.out.println("Change me if you can wtf");
     }
 }
